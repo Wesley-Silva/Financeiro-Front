9d548b904f4718a4af617fac453cf7b59fd2251c
Wesley-Silva <wesleybf3@gmail.com>
2024-02-17 18:13:28 -0300

    Adiciona NavBar ,SideBar e os menus

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DashboardComponent } from "./dashboard.component";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { NavBarModule } from "src/app/components/navbar/navbar.module";
+import { SideBarModule } from "src/app/components/sidebar/sidebar.module";
 
 @NgModule({
     providers:[],
     declarations:[DashboardComponent],
     imports:[
         CommonModule,
-        DashboardRoutingModule
+        DashboardRoutingModule,
+        NavBarModule,
+        SideBarModule
     ]
 })
 
